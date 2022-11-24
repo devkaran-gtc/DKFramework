94ac09b29c854380321acdb24d206402a1015ea6
Devkaran Makvana <devkaran.makvana@gtcsys.com>
2022-11-24 13:14:34 +0530

    Initial Commit

---
+++ DKFramework/DKFramework.h
@@ -0,0 +1,18 @@
+//
+//  DKFramework.h
+//  DKFramework
+//
+//  Created by Devkaran's Mac mini on 24/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DKFramework.
+FOUNDATION_EXPORT double DKFrameworkVersionNumber;
+
+//! Project version string for DKFramework.
+FOUNDATION_EXPORT const unsigned char DKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKFramework/PublicHeader.h>
+
+
